1eb98194b228ade10fdbbec5967ba1825b506759
zxy7010 <3185086801@qq.com>
2019-12-12 23:56:41 -0800

    added a lab1.c file

---
+++ C1/lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+{
+ int celsius,fahr;
+ int lower,upper , step ;
+ lower=0;
+ upper=100;
+ step=5;
+ celsius=lower;
+ while(celsius<=upper){
+ fahr=9*celsius/5+32;
+ printf("%d\t%d\n",celsius,fahr);
+    
+
+ }
+ 
+}
