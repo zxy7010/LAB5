138d7a0ae7f6fb6015e3cb4410edf1f95c4c7968
zxy7010 <3185086801@qq.com>
2019-12-26 19:41:28 -0800

    added a lab7.c file

---
+++ C1/lab7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char*argv[]){
+    int a[100];
+    int i =1;
+    int j = 0;
+    int b=0;
+    int c = 0;
+    int temp;
+    for(i=1;i<argc;i++,j++){
+        a[j]=atof(argv[i]);
+    }
+    for(b=0;b<argc-2;b++){
+     if(a[b]<a[b+1]) 
+{
+temp=a[b];
+a[b]=a[b+1];
+a[b+1]=temp;}
+}
+printf("The min is：%d\n",a[argc-2]);
+return 0;
+}
