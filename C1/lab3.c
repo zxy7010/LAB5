cdbab61b08bad7a149c1278dae5ff52785077c75
zxy7010 <3185086801@qq.com>
2019-12-26 21:14:01 -0800

    added a lab3,c file

--- C1/lab3.c
+++ C1/lab3.c
@@ -2,22 +2,22 @@
 #include<string.h>
 int strindex (char s[], char t[])
 {
-int i,j,k,C, pos=0;
-for(i = 0;s[i] !='\0'; i++){
-for(j = i,k=0; t[k]!='\0'&& s[j]==t[k];j++,k++);
-if(t[k]=='\0'){
-pos=i+1;
-
+    int i,j,k,c,pos=0;
+    for(i = 0;s[i] !='\0'; i++){
+        for(j = i,k=0;t[k]!='\0'&& s[j]==t[k];j++,k++);
+        if(t[k]=='\0'){
+        pos = i+1;
+        c=k;
+    }
+    }
+    if(pos >0)
+    return (pos-1+c-1);
+    return -1;
 }
-}
-if(pos >0)
-return (pos-1+C-1);
-return -1 ;
-}
-main( ){
-char a[100];
-char b[100];
-printf("please input the chain of characters\n");
-scanf("%S %S", a,b);
-printf( "%d\n" ,strindex(b,a));
+main(){
+    char a[100];
+    char b[100];
+    printf("please input the chain of characters\n");
+    scanf("%s %s", a,b);
+    printf("%d\n",strindex(b,a));
 }
